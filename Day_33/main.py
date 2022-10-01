74b25acaf59598bcf955ed46ae2dd13e9cff581a
RobsenGelana <robsengelana21@gmail.com>
2022-10-01 21:55:58 +0300

    get api response

---
+++ Day_33/main.py
@@ -0,0 +1,37 @@
+import requests
+from datetime import datetime
+
+
+LONG = 40.489674
+LAT = 9.145000
+
+# response = requests.get(url='http://api.open-notify.org/iss-now.json')
+# response.raise_for_status()
+
+# data = response.json()
+
+# longitude = data['iss_position']['longitude']
+# latitude = data['iss_position']['latitude']
+
+# iss_position = (longitude, latitude)
+# print(iss_position)
+
+parameters = {
+    'lat': LAT,
+    'lng': LONG,
+    'formatted': 0,
+}
+
+response = requests.get('https://api.sunrise-sunset.org/json', params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise = data['results']['sunrise'].split('T')[1].split(':')[0]
+sunset = data['results']['sunset'].split('T')[1].split(':')[0]
+
+print(sunrise)
+print(sunset)
+
+
+current_time = datetime.now()
+
+
