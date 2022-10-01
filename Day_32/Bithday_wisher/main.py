691b92a9bd9b66cb5fe177f2e424b6467457c7ea
RobsenGelana <robsengelana21@gmail.com>
2022-10-01 13:34:22 +0300

    reading files and replace the placeholder with the person actual name

--- Day_32/Bithday_wisher/main.py
+++ Day_32/Bithday_wisher/main.py
@@ -14,12 +14,20 @@ import random
 from datetime import datetime 
 import smtplib 
 
+PLACEHOLDER = '[NAME]'
+
 today = datetime.now()
 today_tuple = (today.month, today.day)
 
 data = pd.read_csv('./bd.csv')
 birthday_dict= {(data_row['month'], data_row['day']): data_row for index, data_row in data.items()}
 if today_tuple in birthday_dict:
-    pass
+    birthday_name = birthday_dict[today_tuple]
+    file_path = f"./letter_templates/{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        content = letter_file.read()
+        content.replace(PLACEHOLDER, birthday_name['name'])
+        
+
 
 
