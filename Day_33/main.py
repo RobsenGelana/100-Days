c6954b21b5aa057fff506c7363d6116c431022fa
RobsenGelana <robsengelana21@gmail.com>
2022-10-01 22:19:13 +0300

    creating a function that return True if iss is close

--- Day_33/main.py
+++ Day_33/main.py
@@ -1,17 +1,25 @@
 import requests
 from datetime import datetime
+import smtplib
 
 
 LONG = 40.489674
 LAT = 9.145000
 
-response = requests.get(url='http://api.open-notify.org/iss-now.json')
-response.raise_for_status()
+MY_EMAIL = 'example@gmail.com'
+PASSWORD = 'test password'
 
-data = response.json()
+def iss_close():
+    response = requests.get(url='http://api.open-notify.org/iss-now.json')
+    response.raise_for_status()
 
-iss_longitude = float(data['iss_position']['longitude'])
-iss_latitude = float(data['iss_position']['latitude'])
+    data = response.json()
+
+    iss_longitude = float(data['iss_position']['longitude'])
+    iss_latitude = float(data['iss_position']['latitude'])
+
+    if LONG -5  <= iss_longitude <= LONG + 5 and LAT -5 <= iss_latitude <= LAT + 5:
+        return True
 
 
 parameters = {
@@ -23,13 +31,17 @@ parameters = {
 response = requests.get('https://api.sunrise-sunset.org/json', params=parameters)
 response.raise_for_status()
 data = response.json()
-sunrise = data['results']['sunrise'].split('T')[1].split(':')[0]
-sunset = data['results']['sunset'].split('T')[1].split(':')[0]
-
-print(sunrise)
-print(sunset)
-
+sunrise = int(data['results']['sunrise'].split('T')[1].split(':')[0])
+sunset = int(data['results']['sunset'].split('T')[1].split(':')[0])
 
 current_time = datetime.now()
+
+
+with smtplib.SMTP('YOUR EMAIL PROVIDER SMTP SERVER ADDRESS') as connection:
+    connection.starttls()
+    connection.login(MY_EMAIL, PASSWORD)
+    connection.sendmail(from_addr=MY_EMAIL, to_addrs='test@gmail.com', msg='LOOK UP')
+
+
 
 
