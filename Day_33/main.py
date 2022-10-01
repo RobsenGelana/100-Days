def12064bfc597d4c0b4f5250b0bafe9c9cd213b
RobsenGelana <robsengelana21@gmail.com>
2022-10-01 22:21:13 +0300

    creating a function that return True if current time is night time

--- Day_33/main.py
+++ Day_33/main.py
@@ -21,26 +21,28 @@ def iss_close():
     if LONG -5  <= iss_longitude <= LONG + 5 and LAT -5 <= iss_latitude <= LAT + 5:
         return True
 
+def is_night():
+    parameters = {
+        'lat': LAT,
+        'lng': LONG,
+        'formatted': 0,
+    }
 
-parameters = {
-    'lat': LAT,
-    'lng': LONG,
-    'formatted': 0,
-}
+    response = requests.get('https://api.sunrise-sunset.org/json', params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data['results']['sunrise'].split('T')[1].split(':')[0])
+    sunset = int(data['results']['sunset'].split('T')[1].split(':')[0])
 
-response = requests.get('https://api.sunrise-sunset.org/json', params=parameters)
-response.raise_for_status()
-data = response.json()
-sunrise = int(data['results']['sunrise'].split('T')[1].split(':')[0])
-sunset = int(data['results']['sunset'].split('T')[1].split(':')[0])
-
-current_time = datetime.now()
+    current_time = datetime.now().hour
+    if current_time >= sunset or current_time <= sunrise:
+        return True
 
 
-with smtplib.SMTP('YOUR EMAIL PROVIDER SMTP SERVER ADDRESS') as connection:
-    connection.starttls()
-    connection.login(MY_EMAIL, PASSWORD)
-    connection.sendmail(from_addr=MY_EMAIL, to_addrs='test@gmail.com', msg='LOOK UP')
+# with smtplib.SMTP('YOUR EMAIL PROVIDER SMTP SERVER ADDRESS') as connection:
+#     connection.starttls()
+#     connection.login(MY_EMAIL, PASSWORD)
+#     connection.sendmail(from_addr=MY_EMAIL, to_addrs='test@gmail.com', msg='LOOK UP')
 
 
 
