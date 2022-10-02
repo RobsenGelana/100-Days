bd48efb1b67b863a92da9749166e2e6c158e40d6
RobsenGelana <robsengelana21@gmail.com>
2022-10-02 21:09:15 +0300

    creating a canvas

--- Day_34/ui.py
+++ Day_34/ui.py
@@ -6,7 +6,11 @@ class QuizInterface:
     def __init__(self) -> None:
         self.window = Tk()
         self.window.title('Quizzler')
-        self.canvas = Canvas()
+        self.window.config(padx=20, pady=20)
+
+        self.canvas = Canvas(height=250, width=300)
+        self.canvas.config(bg='white')
+        self.canvas.grid(column=0, row=1, columnspan=2)
 
         self.true_image = PhotoImage(file='~/Desktop/100Days/Day_34/images/true.png')
         self.true = Button()
