4e659e58fbb8c5dbf18ac87b57439dd984c3161a
RobsenGelana <robsengelana21@gmail.com>
2022-10-02 20:41:49 +0300

    my commit

--- Day_34/ui.py
+++ Day_34/ui.py
@@ -1,2 +1,22 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
 
+class QuizInterface:
+    def __init__(self) -> None:
+        self.window = Tk()
+        self.window.title('Quizzler')
+        self.canvas = Canvas()
+
+        self.true_image = PhotoImage(file='~/Desktop/100Days/Day_34/images/true.png')
+        self.true = Button()
+        self.true.config(image=self.true_image)
+        self.true.grid(row=3, column=0)
+        self.false_image = PhotoImage(file='~/Desktop/100Days/Day_34/images/false.png')
+        self.false = Button()
+        self.false.config(image=self.false_image)
+        self.false.grid(row=)
+
+
+        
+        self.window.mainloop()
