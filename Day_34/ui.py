7c78fd06bb855848bfee1842a7620bedb706edfc
RobsenGelana <robsengelana21@gmail.com>
2022-10-04 07:33:00 +0300

    adding a score label on user interface

--- Day_34/ui.py
+++ Day_34/ui.py
@@ -8,18 +8,21 @@ class QuizInterface:
         self.window = Tk()
         self.window.title('Quizzler')
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
-        self.canvas = Canvas(height=250, width=300)
-        self.canvas.config(bg='white')
-        self.text = self.canvas.create_text(125, 150, text="Title", font=FONT)
-        self.canvas.grid(column=0, row=1, columnspan=2)
 
-        self.true_image = PhotoImage(file='~/Desktop/100Days/Day_34/images/true.png')
+        self.score_label = Label(text='Score: 0', fg='white', bg=THEME_COLOR)
+        self.score_label.grid(row=0, column=1)
+
+        self.canvas = Canvas(width=300, height=250,  bg='white')
+        self.question_text = self.canvas.create_text(150, 125,text="Question Text", fill=THEME_COLOR, font=('Arial', 20, 'normal'))
+        self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
+
+        true_image = PhotoImage(file='~/Desktop/100Days/Day_34/images/true.png')
         self.true = Button()
-        self.true.config(image=self.true_image)
+        self.true.config(image=true_image)
         self.true.grid(row=3, column=0)
-        self.false_image = PhotoImage(file='~/Desktop/100Days/Day_34/images/false.png')
+        false_image = PhotoImage(file='~/Desktop/100Days/Day_34/images/false.png')
         self.false = Button()
-        self.false.config(image=self.false_image)
+        self.false.config(image=false_image)
         self.false.grid(row=3, column=1)
 
 
