d05d1eebe7a6f5cb309ca6038adbf5c68699db40
RobsenGelana <robsengelana21@gmail.com>
2022-10-10 22:19:48 +0300

    printing good news if the diff perecentage is greater than 5

--- Day_36/main.py
+++ Day_36/main.py
@@ -25,12 +25,12 @@ day_before_yesterday_data = data_list[1]
 day_before_yesterday_data_closing_price = day_before_yesterday_data['4. close']
 #TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
 difference = abs(float(yesterday_data_closing_price) - float(day_before_yesterday_data_closing_price))
-print(difference)
-
 #TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
-
+diff_percentage = (difference / float(yesterday_data_closing_price)) * 100
+# print(diff_percentage)
 #TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
-
+if diff_percentage > 5:
+    print("Great News")
     ## STEP 2: https://newsapi.org/ 
     # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
 
