78cb388f1f9d80165ba7d0c3952dda2ded797812
RobsenGelana <robsengelana21@gmail.com>
2022-10-11 13:44:04 +0300

    adding my news api

--- Day_36/main.py
+++ Day_36/main.py
@@ -2,7 +2,7 @@ import requests
 
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
-
+NEWS_API = "6443794787c24c238652f58bf84c693b"
 parameters = {
     "function":"TIME_SERIES_DAILY",
     "apikey":"1MIVJZMBYNROCAWB",
@@ -27,12 +27,12 @@ difference = abs(float(yesterday_data_closing_price) - float(day_before_yesterda
 
 diff_percentage = (difference / float(yesterday_data_closing_price)) * 100
 
-if diff_percentage > 5:
-    print("Great News")
     ## STEP 2: https://newsapi.org/ 
     # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
 
 #TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
+if diff_percentage > 0:
+    print("Great News")
 
 #TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
 
