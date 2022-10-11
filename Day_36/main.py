4e71789363fb7cee8483eb2ef45258ddcd023eb2
RobsenGelana <robsengelana21@gmail.com>
2022-10-11 14:10:29 +0300

    getting the first three articles

--- Day_36/main.py
+++ Day_36/main.py
@@ -40,10 +40,11 @@ if diff_percentage > 0:
     }
 
     news_response = requests.get(NEWS_ENDPOINT, params=news_parameter)
-    news_article = news_response.json()['articles']
+    article = news_response.json()['articles']
 
-#TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
-
+    # Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
+    three_articles = article[:3]
+    print(three_articles)
     ## STEP 3: Use twilio.com/docs/sms/quickstart/python
     #to send a separate message with each article's title and description to your phone number. 
 
