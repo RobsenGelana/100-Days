ac6e87fae4546aabf42d22dd4640b03a40e0deba
RobsenGelana <robsengelana21@gmail.com>
2022-10-11 14:26:54 +0300

    formating the output

--- Day_36/main.py
+++ Day_36/main.py
@@ -48,9 +48,9 @@ if diff_percentage > 0:
     ## STEP 3: Use twilio.com/docs/sms/quickstart/python
     #to send a separate message with each article's title and description to your phone number. 
 
-#TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
-
-#TODO 9. - Send each article as a separate message via Twilio. 
+    #Create a new list of the first 3 article's headline and description using list comprehension.
+    formatted_article = [f"Headline: {article['title']}. \nBrief: {article['description']}" for article in three_articles]
+    #TODO 9. - Send each article as a separate message via Twilio. 
 
 
 
