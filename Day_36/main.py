8c885bfe49a03ddb35619f899bbc3148b88fb407
RobsenGelana <robsengelana21@gmail.com>
2022-10-11 14:06:11 +0300

    adding comments

--- Day_36/main.py
+++ Day_36/main.py
@@ -16,32 +16,33 @@ NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 response = requests.get(url=STOCK_ENDPOINT, params=parameters)
 response.raise_for_status()
 data = response.json()['Time Series (Daily)']
+#Get yesterday's closing stock price. Hint: You can perform list comprehensions on Python dictionaries. e.g. [new_value for (key, value) in dictionary.items()]
+
 data_list = [value for key, value in data.items()]
 yesterday_data = data_list[0]
 yesterday_data_closing_price = yesterday_data['4. close']
-
+#Get the day before yesterday's closing stock price
 day_before_yesterday_data = data_list[1]
 day_before_yesterday_data_closing_price = day_before_yesterday_data['4. close']
-
+#Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
 difference = abs(float(yesterday_data_closing_price) - float(day_before_yesterday_data_closing_price))
-
+#Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
 diff_percentage = (difference / float(yesterday_data_closing_price)) * 100
 
     ## STEP 2: https://newsapi.org/ 
     # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
 
-#TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
+#Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
 if diff_percentage > 0:
     news_parameter = {
         'apiKey':NEWS_API,
         'qInTitle':COMPANY_NAME,
     }
 
-
-    requests.get(NEWS_ENDPOINT, params=news_parameter)
+    news_response = requests.get(NEWS_ENDPOINT, params=news_parameter)
+    news_article = news_response.json()['articles']
 
 #TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
-
 
     ## STEP 3: Use twilio.com/docs/sms/quickstart/python
     #to send a separate message with each article's title and description to your phone number. 
