c4aa55d1d22658165b28a95697dd002f23f9528d
RobsenGelana <robsengelana21@gmail.com>
2022-11-08 21:10:09 +0300

    adding a constant variable

--- Day_38/main.py
+++ Day_38/main.py
@@ -2,7 +2,10 @@ import requests
 
 APP_ID = "a8daa4e2"
 APP_KEY = "6d1fcdaf606cafdb8ba55a5a036ea703"
-
+GENDER = 'male'
+WEIGHT = 68
+HEIGHT = 173
+AGE = 19
 
 page_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 user_input = input("Tell me which exercises you did: ")
@@ -14,10 +17,10 @@ http_header = {
 
 json_params = {
     "query": user_input,
-    "gender": "male",
-    "weight_kg": 68,
-    "height_cm": 173,
-    "age": 19
+    "gender": GENDER,
+    "weight_kg": WEIGHT,
+    "height_cm": HEIGHT,
+    "age": AGE
 }
 
 response = requests.post(url=page_endpoint, json=json_params, headers=http_header)
