746489adfc7b838b5edbfe8cc03f96d500b40da1
RobsenGelana <robsengelana21@gmail.com>
2022-11-08 11:03:41 +0300

    creating parmas for the api reqeust

--- Day_37/main.py
+++ Day_37/main.py
@@ -1,14 +1,24 @@
 import requests
 
+TOKEN = "asdfhafw1312jasdfa"
+USERNAME = "robsen"
 endpoint = "https://pixe.la/v1/users"
 
 user_params = {
-    "token": "asdfhafw1312jasdfa",
-    "username": "robsen",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
 
-response = requests.post(url=endpoint, json=user_params)
+# response = requests.post(url=endpoint, json=user_params)
 #test code
-print(response.text)
+# print(response.text)
+
+graph_endpoint = f"{endpoint}/{USERNAME}/graphs"
+
+graph_conf = {
+    
+}
+
+requests.post()
