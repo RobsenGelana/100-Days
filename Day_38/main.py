b7e6a14c9bd40134299a25b496a36eedecababf9
RobsenGelana <robsengelana21@gmail.com>
2022-11-08 21:01:57 +0300

    adding the json parmas

--- Day_38/main.py
+++ Day_38/main.py
@@ -5,9 +5,17 @@ APP_KEY = "6d1fcdaf606cafdb8ba55a5a036ea703"
 
 
 page_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+user_input = input("Tell me which exercises you did: ")
 
 http_header = {
     "x-app-id": APP_ID,
     'x-app-key': APP_KEY,
 }
 
+json_params = {
+    "query": user_input,
+    "gender": "male",
+    "weight_kg": 68,
+    "height_cm": 173,
+    "age": 19
+}
