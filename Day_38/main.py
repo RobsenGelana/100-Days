4ff8a78480c0a1b19209c1001f704935ea9f8ebd
RobsenGelana <robsengelana21@gmail.com>
2022-11-09 09:45:51 +0300

    sending input to goolge sheet

--- Day_38/main.py
+++ Day_38/main.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 
 APP_ID = "a8daa4e2"
 APP_KEY = "6d1fcdaf606cafdb8ba55a5a036ea703"
@@ -7,6 +8,7 @@ WEIGHT = 68
 HEIGHT = 173
 AGE = 19
 
+sheety_endpoint = "https://api.sheety.co/9b7ed0569a8dbb7098467e7c1f5484dc/myWorkOut/workouts"
 page_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 user_input = input("Tell me which exercises you did: ")
 
@@ -24,4 +26,22 @@ json_params = {
 }
 
 response = requests.post(url=page_endpoint, json=json_params, headers=http_header)
-print(response.text)
+result = response.json()
+
+
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
+
+for exercise in result['exercise']:
+    sheety_input = {
+        "workout": {
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+    sheet_response = requests.post(sheety_endpoint, json=sheety_input)
+
+    print(sheet_response.text)
