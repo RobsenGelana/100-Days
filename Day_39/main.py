dc6f0200b672b3079b6b092d5d6d2a0755d36aa1
RobsenGelana <robsengelana21@gmail.com>
2022-11-09 18:17:40 +0300

    getting data from the sheety api

--- Day_39/main.py
+++ Day_39/main.py
@@ -1,1 +1,7 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+import requests
+from pprint import pprint
+sheety_endpoint = "https://api.sheety.co/9b7ed0569a8dbb7098467e7c1f5484dc/copyOfFlightDeals/prices"
+
+response = requests.get(url=sheety_endpoint)
+pprint(response.text)
