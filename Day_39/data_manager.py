893cfe8e5e903b575d023846e94af4c4c6db03f1
RobsenGelana <robsengelana21@gmail.com>
2022-11-09 18:48:34 +0300

    creating class based request

--- Day_39/data_manager.py
+++ Day_39/data_manager.py
@@ -1,3 +1,9 @@
+import requests
+
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    pass
+    sheety_endpoint = "https://api.sheety.co/9b7ed0569a8dbb7098467e7c1f5484dc/copyOfFlightDeals/prices"
+
+    response = requests.get(url=sheety_endpoint)
+    print(response.text)
+
--- Day_39/main.py
+++ Day_39/main.py
@@ -1,7 +1,5 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
-import requests
-from pprint import pprint
-sheety_endpoint = "https://api.sheety.co/9b7ed0569a8dbb7098467e7c1f5484dc/copyOfFlightDeals/prices"
+from data_manager import DataManager
 
-response = requests.get(url=sheety_endpoint)
-pprint(response.text)
+sheet_data = DataManager()
+
