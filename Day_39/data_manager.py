a8669fb9e4cc25b0d457a2d8bfd5a44ebcda1f5f
RobsenGelana <robsengelana21@gmail.com>
2022-11-10 14:20:22 +0300

    my commit

--- Day_39/data_manager.py
+++ Day_39/data_manager.py
@@ -3,14 +3,31 @@ import requests
 sheety_endpoint = "https://api.sheety.co/9b7ed0569a8dbb7098467e7c1f5484dc/copyOfFlightDeals/prices"
 
 class DataManager:
+    
     def __init__(self):
-        self.initial = {}
-    #This class is responsible for talking to the Google Sheet.
-    def get_initial_data(self):
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        # 2. Use the Sheety API to GET all the data in that sheet and print it out.
         response = requests.get(url=sheety_endpoint)
         data = response.json()
-        self.initial = data['prices']
-        return self.initial
+        self.destination_data = data["prices"]
+        # 3. Try importing pretty print and printing the data out again using pprint().
+        # pprint(data)
+        return self.destination_data
 
 
-
+    # 6. In the DataManager Class make a PUT request and use the row id  from sheet_data
+    # to update the Google Sheet with the IATA codes. (Do this using code).
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(
+                url=f"{sheety_endpoint}/{city['id']}",
+                json=new_data
+            )
+            print(response.text)
--- Day_39/flight_search.py
+++ Day_39/flight_search.py
@@ -1,3 +1,17 @@
+import requests
+
+TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
+TEQUILA_API_KEY = "1ULV6FW3pPxLT2cpq_uG-90B3THnL-kh"
+
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
-    pass
+    def __init__(self) -> None:
+        pass
+    def get_destination_code(self, city_name):
+        location_endpoint = f"{TEQUILA_ENDPOINT}/locations/query"
+        headers = {"apikey": TEQUILA_API_KEY}
+        query = {"term": city_name, "location_types": "city"}
+        response = requests.get(url=location_endpoint, headers=headers, params=query)
+        results = response.json()["locations"]
+        code = results[0]["code"]
+        return code
