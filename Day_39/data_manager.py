0b582385674a0c72b5753b5e20fa5d3cdd317381
RobsenGelana <robsengelana21@gmail.com>
2022-11-10 13:16:50 +0300

    creating a def in data_manager

--- Day_39/data_manager.py
+++ Day_39/data_manager.py
@@ -1,9 +1,15 @@
 import requests
 
-class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    sheety_endpoint = "https://api.sheety.co/9b7ed0569a8dbb7098467e7c1f5484dc/copyOfFlightDeals/prices"
+sheety_endpoint = "https://api.sheety.co/9b7ed0569a8dbb7098467e7c1f5484dc/copyOfFlightDeals/prices"
 
-    response = requests.get(url=sheety_endpoint)
-    print(response.text)
+class DataManager:
+    def __init__(self):
+        pass
+    #This class is responsible for talking to the Google Sheet.
+    def get_initial_data(self):
+        response = requests.get(url=sheety_endpoint)
+        data = response.json()
+        print(data)
+
+DataManager()
 
