3774025ee9bb3b95b15630d6a95e0e93cfd41e91
RobsenGelana <robsengelana21@gmail.com>
2022-11-10 14:03:41 +0300

    fixing bug

--- Day_39/data_manager.py
+++ Day_39/data_manager.py
@@ -4,12 +4,13 @@ sheety_endpoint = "https://api.sheety.co/9b7ed0569a8dbb7098467e7c1f5484dc/copyOf
 
 class DataManager:
     def __init__(self):
-        pass
+        self.initial = {}
     #This class is responsible for talking to the Google Sheet.
     def get_initial_data(self):
         response = requests.get(url=sheety_endpoint)
         data = response.json()
-        print(data)
+        self.initial = data['prices']
+        return self.initial
 
-DataManager()
+
 
--- Day_39/main.py
+++ Day_39/main.py
@@ -1,5 +1,7 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 from data_manager import DataManager
 
-sheet_data = DataManager()
+data_manager = DataManager()
 
+sheet_data = data_manager.get_initial_data()
+print(sheet_data)
