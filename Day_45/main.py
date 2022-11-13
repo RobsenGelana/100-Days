98e3ed670f27b2ee0588152e6483cdf23869e5f4
RobsenGelana <robsengelana21@gmail.com>
2022-11-13 08:41:29 +0300

    completing the chalenge

--- Day_45/main.py
+++ Day_45/main.py
@@ -9,7 +9,9 @@ yc_web_page = response.text
 soup = BeautifulSoup(yc_web_page, "html.parser")
 first_article = soup.find(name="a", class_="storylink")
 
-print(first_article.getText())
+article_text = first_article.getText()
+article_link = first_article.get("href")
+article_votes = soup.find(name='span', class_="score").getText()
 
 
 
