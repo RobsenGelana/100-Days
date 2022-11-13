84fc3c507ec16c8f508a7454e6520c392c2f9075
RobsenGelana <robsengelana21@gmail.com>
2022-11-13 14:29:57 +0300

    fixing bug

---
+++ Day_45/final.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+import requests
+
+response = requests.get(url="https://www.empireonline.com/movies/features/best-movies-2/")
+movies_html = response.text
+
+soup = BeautifulSoup(movies_html, 'html.parser')
+all_movies = soup.find_all(name="h3", class_="jsx-4245974604")
+
+movie_title = [movie.getText() for movie in all_movies]
+print(movie_title)
