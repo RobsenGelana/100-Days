53e3b563bfdfe3ab43635f31f048e72db089e632
RobsenGelana <robsengelana21@gmail.com>
2022-11-13 08:20:53 +0300

    getting a text response from a live website

--- Day_45/main.py
+++ Day_45/main.py
@@ -1,22 +1,54 @@
 from bs4 import BeautifulSoup
+import requests
 
-with open('website.html', 'r') as file:
-    contents = file.read()
+#Using BeautifulSoup with live website
+
+response = requests.get("https://news.ycombinator.com/newest")
+print(response.text)
 
 
-soup = BeautifulSoup(contents, 'html.parser')
-# print(soup.title.string)
 
-#Finding all tags 
-all_anchor_tags = soup.find_all(name="a")
-# print(all_anchor_tags)
 
-# for tags in all_anchor_tags:
-#     #getting all the text
-#     # print(tags.getText())
-#     #getting the link
-#     print(tags.get("href"))
 
-#finding tags with  class 
-heading = soup.find(name="h3", class_="heading")
-print(heading)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# with open('website.html', 'r') as file:
+#     contents = file.read()
+
+
+# soup = BeautifulSoup(contents, 'html.parser')
+# # print(soup.title.string)
+
+# #Finding all tags 
+# all_anchor_tags = soup.find_all(name="a")
+# # print(all_anchor_tags)
+
+# # for tags in all_anchor_tags:
+# #     #getting all the text
+# #     # print(tags.getText())
+# #     #getting the link
+# #     print(tags.get("href"))
+
+# #finding tags with  class 
+# heading = soup.find(name="h3", class_="heading")
+# print(heading)
