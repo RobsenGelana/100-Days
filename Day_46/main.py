c15e0f4630ff499b93e780406d669fad90b59b08
RobsenGelana <robsengelana21@gmail.com>
2022-11-13 15:56:20 +0300

    fixing bug

--- Day_46/main.py
+++ Day_46/main.py
@@ -3,11 +3,12 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 
 
-URL = 'https://www.billboard.com/charts/hot-100/'
+user_input = input("what year you would like to travel to in YYY-MM-DD: ")
+URL = 'https://www.billboard.com/charts/hot-100/' + user_input
 response = requests.get(url=URL)
 music_html = response.text
 
 soup = BeautifulSoup(music_html, 'html.parser')
-music_list = soup.find(name="use")
-
-# user_input = input("what year you would like to travel to in YYY-MM-DD: ")
+music_list = soup.find_all(name="h3", class_="c-title")
+songs = [music.getText().strip() for music in music_list]
+print(songs)
