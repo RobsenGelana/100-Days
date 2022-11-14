7646f7e057a9e28cfd522490af958959fcadc601
RobsenGelana <robsengelana21@gmail.com>
2022-11-14 15:07:49 +0300

    commeting a demo code

--- Day_46/main.py
+++ Day_46/main.py
@@ -18,17 +18,17 @@
 # musics = [songs.text.strip() for songs in songs_list]
 # print(musics)
 
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+# import spotipy
+# from spotipy.oauth2 import SpotifyClientCredentials
 
-birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
-spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+# birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
+# spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
-results = spotify.artist_albums(birdy_uri, album_type='album')
-albums = results['items']
-while results['next']:
-    results = spotify.next(results)
-    albums.extend(results['items'])
+# results = spotify.artist_albums(birdy_uri, album_type='album')
+# albums = results['items']
+# while results['next']:
+#     results = spotify.next(results)
+#     albums.extend(results['items'])
 
-for album in albums:
-    print(album['name'])
+# for album in albums:
+#     print(album['name'])
