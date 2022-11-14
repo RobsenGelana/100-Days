af748f5c874bed89c0b8825db009c9fb49afed8b
RobsenGelana <robsengelana21@gmail.com>
2022-11-14 18:55:17 +0300

    automating to send email

--- Day_47/main.py
+++ Day_47/main.py
@@ -1,3 +1,4 @@
+import smtplib
 import requests
 import lxml
 from bs4 import BeautifulSoup
@@ -17,3 +18,20 @@ price = soup.find(id="priceblock_ourprice").get_text()
 price_without_currency = price.split("$")[1]
 price_as_float = float(price_without_currency)
 print(price_as_float)
+
+title = soup.find(id="productTitle").get_text().strip()
+print(title)
+
+BUY_PRICE = 200
+
+if price_as_float < BUY_PRICE:
+    message = f"{title} is now {price}"
+
+    with smtplib.SMTP(YOUR_SMTP_ADDRESS, port=587) as connection:
+        connection.starttls()
+        result = connection.login(YOUR_EMAIL, YOUR_PASSWORD)
+        connection.sendmail(
+            from_addr=YOUR_EMAIL,
+            to_addrs=YOUR_EMAIL,
+            msg=f"Subject:Amazon Price Alert!\n\n{message}\n{url}"
+        )
