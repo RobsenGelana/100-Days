b1e0440c16ad11719763e9a69c6bd2da128fc7b3
RobsenGelana <robsengelana21@gmail.com>
2022-11-16 19:13:13 +0300

    my commit

--- Day_48/intraction.py
+++ Day_48/intraction.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 chrome_driver_path= "/home/robinson/Documents/Development/chromedriver"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
@@ -7,4 +8,4 @@ driver = webdriver.Chrome(executable_path=chrome_driver_path)
 driver.get("https://en.wikipedia.org/wiki/Main_Page")
 
 number = driver.find_element(By.CSS_SELECTOR, "#articlecount a")
-print(number.text)
+number.click()
