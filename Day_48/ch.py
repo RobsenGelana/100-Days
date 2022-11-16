a79c08821326d89f5916ca0d2d07bc6ebfb9e5a3
RobsenGelana <robsengelana21@gmail.com>
2022-11-16 18:05:47 +0300

    getting the event data and title

--- Day_48/ch.py
+++ Day_48/ch.py
@@ -5,9 +5,12 @@ chrome_driver_path= "/home/robinson/Documents/Development/chromedriver"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
 driver.get("https://www.python.org/")
-events_html = driver.find_elements(By.CSS_SELECTOR, "time")
+events_html = driver.find_elements(By.CSS_SELECTOR, ".event-widget time")
 events_date = [event.text for event in events_html]
 print(events_date)
+event_title = driver.find_elements(By.CSS_SELECTOR, ".event-widget a")
+event_data = [data.text for data in event_title]
+print(event_data)
 event_dict = {}
 
 
