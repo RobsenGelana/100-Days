45b4f7c6df42e1d302ae818c34cfe382c708d97a
RobsenGelana <robsengelana21@gmail.com>
2022-11-16 17:03:35 +0300

    test code with diffrent url

--- Day_48/main.py
+++ Day_48/main.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 chrome_driver_path= "/home/robinson/Documents/Development/chromedriver"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
-driver.get("https://www.amazon.com/JBL-VIBE-100-TWS-Ear/dp/B095J56XV5/ref=sr_1_3?crid=VSPD71L344NY&keywords=wireless+earbuds&qid=1668606840&sprefix=wire%2Caps%2C465&sr=8-3")
-price = driver.find_element(By.CLASS_NAME, "a-offscreen")
+driver.get("https://www.amazon.com/Dell-XPS-9310-Laptop-Touchscreen/dp/B0BB1HF9WY/ref=sr_1_4?keywords=dell+xps+13&qid=1668607207&sprefix=dell%2Caps%2C569&sr=8-4")
+price = driver.find_element(By.CLASS_NAME, "a-price-whole")
 print(price.text)
 driver.quit()
