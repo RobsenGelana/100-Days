c576ef0c4a915995b7ff1c7d36226404179e59b3
RobsenGelana <robsengelana21@gmail.com>
2022-11-16 19:29:43 +0300

    completing the cha

---
+++ Day_48/ch2.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+chrome_driver_path= "/home/robinson/Documents/Development/chromedriver"
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
+
+driver.get("http://secure-retreat-92358.herokuapp.com/")
+first_name = driver.find_element(By.NAME, "fName")
+last_name = driver.find_element(By.NAME, "lName")
+email = driver.find_element(By.NAME, "email")
+button = driver.find_element(By.CSS_SELECTOR, ".btn")
+
+first_name.send_keys("Robsen")
+last_name.send_keys("gelana")
+email.send_keys("test@gmail.com")
+button.send_keys(Keys.ENTER)
+
+# driver.quit()
