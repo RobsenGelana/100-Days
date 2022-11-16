ece8d0910253514d8995dd81742e51be8a33137c
RobsenGelana <robsengelana21@gmail.com>
2022-11-16 18:07:25 +0300

    fixing error

--- Day_48/ch.py
+++ Day_48/ch.py
@@ -8,7 +8,7 @@ driver.get("https://www.python.org/")
 events_html = driver.find_elements(By.CSS_SELECTOR, ".event-widget time")
 events_date = [event.text for event in events_html]
 print(events_date)
-event_title = driver.find_elements(By.CSS_SELECTOR, ".event-widget a")
+event_title = driver.find_elements(By.CSS_SELECTOR, ".event-widget li a")
 event_data = [data.text for data in event_title]
 print(event_data)
 event_dict = {}
