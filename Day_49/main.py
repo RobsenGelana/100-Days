d991fddab3721538d4eafd8213d3efd59dba07f3
RobsenGelana <robsengelana21@gmail.com>
2022-11-18 18:02:00 +0300

    getting the url

--- Day_49/main.py
+++ Day_49/main.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import time 
 
 chrome_driver_path= "/home/robinson/Documents/Development/chromedriver"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
+
+driver.get("https://www.linkedin.com/jobs/search/?f_LF=f_AL&geoId=102257491&keywords=python%20developer&location=London%2C%20England%2C%20United%20Kingdom&redirect=false&position=1&pageNum=0")
