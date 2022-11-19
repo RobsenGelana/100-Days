1c556cb8502adfbb8761380eab02568881c5231e
RobsenGelana <robsengelana21@gmail.com>
2022-11-19 15:46:14 +0300

    mycommit

--- Day_50/main.py
+++ Day_50/main.py
@@ -17,3 +17,20 @@ login_page.click()
 time.sleep(2)
 fb_login = driver(By.XPATH,  '//*[@id="modal-manager"]/div/div/div[1]/div/div[3]/span/div[2]/button')
 fb_login.click()
+
+time.sleep(2)
+base_window = driver.window_handles[0]
+fb_login_window = driver.window_handles[1]
+driver.switch_to.window(fb_login_window)
+print(driver.title)
+
+#Login and hit enter
+email = driver(By.XPATH, '//*[@id="email"]')
+password = driver(By.XPATH, '//*[@id="pass"]')
+email.send_keys(FACEBOOK_USERNAME)
+password.send_keys(FACEBOOK_PASSWORD)
+password.send_keys(Keys.ENTER)
+
+#Switch back to Tinder window
+driver.switch_to.window(base_window)
+print(driver.title)
