2c7321803cd36a9d144b18d1ac884211d7672496
RobsenGelana <robsengelana21@gmail.com>
2022-11-19 15:41:17 +0300

    my commit

--- Day_50/main.py
+++ Day_50/main.py
@@ -5,3 +5,10 @@ from selenium.webdriver.common.keys import Keys
 
 chrome_driver_path= "/home/robinson/Documents/Development/chromedriver"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
+
+driver.get("https://tinder.com")
+login_page = driver(By.XPATH, '//*[@id="q798806120"]/div/div[1]/div/div/main/div/div[2]/div/div[3]/div/div/button[2]/div[2]/div[2]')
+login_page.click()
+
+while True:
+    time.sleep(6)
