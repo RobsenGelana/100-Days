74a00c99969d61630e8264bb33945e68cfe88d2a
RobsenGelana <robsengelana21@gmail.com>
2022-11-19 15:43:51 +0300

    sign in with facebook

--- Day_50/main.py
+++ Day_50/main.py
@@ -3,12 +3,17 @@ import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
+FACEBOOK_USERNAME = "YOUR FACEBOOK USER NAME"
+FACEBOOK_PASSWORD = "YOUR FACEBOOK PASSWORD"
+
 chrome_driver_path= "/home/robinson/Documents/Development/chromedriver"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
 driver.get("https://tinder.com")
+time.sleep(2)
 login_page = driver(By.XPATH, '//*[@id="q798806120"]/div/div[1]/div/div/main/div/div[2]/div/div[3]/div/div/button[2]/div[2]/div[2]')
 login_page.click()
 
-while True:
-    time.sleep(6)
+time.sleep(2)
+fb_login = driver(By.XPATH,  '//*[@id="modal-manager"]/div/div/div[1]/div/div[3]/span/div[2]/button')
+fb_login.click()
