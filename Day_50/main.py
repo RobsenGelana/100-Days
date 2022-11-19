41aa6ecbb254ffb7b249d7059316d4eed571f78b
RobsenGelana <robsengelana21@gmail.com>
2022-11-19 15:31:09 +0300

    importing modules

---
+++ Day_50/main.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+chrome_driver_path= "/home/robinson/Documents/Development/chromedriver"
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
